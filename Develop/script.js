34959a2e35d778c0f01df1f876601e92421a00fd
unknown <berrytmt612@gmail.com>
2020-06-22 21:17:20 -0500

    late late for an important date

--- Develop/script.js
+++ Develop/script.js
@@ -4,11 +4,11 @@ var lowerCaseChar = "abcdefghijklmnopqrstuvwxyz";
 var numChars = "0123456789";
 var specialChars = "~!@#$%^&*'`?()_-=+,/[]{|}<>:;";
 
-// Get references to the #generate element
+
 var generateBtn = document.querySelector("#generate");
 
 var generatePassword = function() {
-  // variable defines password label which will generate later on based on following variables if selected by user
+ 
   var password = "";
 
   var passLength = window.prompt("How many characters would you like your password to be? (Choose between 8-128 character length).");
@@ -16,53 +16,53 @@ var generatePassword = function() {
     return;
   }
   
-  // while passLength is Not-A-Number, user is prompted to pick a value of 8 - 128 
+  
   while (isNaN(passLength)) {
     passLength = window.prompt("Please choose a password length of 8 - 128 characters.");
   }
 
-  // input for password length forced into a number
+  
   passLength = parseInt(passLength, 10); 
 
-  // ensures user enters a password length 
+
   while (passLength < 8 || passLength > 128 || isNaN(passLength)) {
     passLength = window.prompt("Length MUST be 8 - 128 characters. How many characters do you wish your password to be?")
   }
 
-  // input for password character types
+  
   if (passLength >= 8 && passLength <= 128) {
-    var upperCaseQ = window.confirm("Do you wish to include CAPITAL letters?");
-    var lowerCaseQ = window.confirm("Do you wish to include LOWER CASE letters?");
-    var availNumsQ = window.confirm("Do you wish to include NUMERICAL characters?");
-    var availSpecsQ = window.confirm("Do you wish to include SPECIAL characters?");
+    var upperCase = window.confirm("Do you wish to include CAPITAL letters?");
+    var lowerCase = window.confirm("Do you wish to include LOWER CASE letters?");
+    var availNums = window.confirm("Do you wish to include NUMBER characters?");
+    var availSpecs = window.confirm("Do you wish to include SPECIAL characters?");
     
-    // forces user to pick at least one character type to continue
-    while (!upperCaseQ && !lowerCaseQ && !availNumsQ && !availSpecsQ) {
-      // loop of warning and prompts until at least one character type selected
-      window.confirm("WARNING: You must choose at least one character type to continue.");
-      upperCaseQ = window.confirm("Do you wish to include CAPITAL letters?");
-      lowerCaseQ = window.confirm("Do you wish to include LOWER CASE letters?");
-      availNumsQ = window.confirm("Do you wish to include NUMERICAL characters?");
-      availSpecsQ = window.confirm("Do you wish to include SPECIAL characters?");
+    
+    while (!upperCase && !lowerCase && !availNums && !availSpecs) {
+      
+      window.confirm("ALERT: You must choose at least one character type to continue.");
+      upperCase = window.confirm("Do you wish to include CAPITAL letters?");
+      lowerCase = window.confirm("Do you wish to include LOWER CASE letters?");
+      availNums = window.confirm("Do you wish to include NUMBER characters?");
+      availSpecs = window.confirm("Do you wish to include SPECIAL characters?");
     }
 
-    // specific variables user requested into password if confirmation received
+    
     var createPassword = '';
 
-    if (upperCaseQ) {
-      createPassword += upperCaseChar; // if upperCaseQ selected, introduce upperCaseChar characters to createPassword
+    if (upperCase) {
+      createPassword += upperCaseChar; 
     }
 
-    if (lowerCaseQ) {
-      createPassword += lowerCaseChar; // if lowerCaseChar selected, introduce lowerCaseChar characters to createPassword
+    if (lowerCase) {
+      createPassword += lowerCaseChar; 
     }
 
-    if (availNumsQ) {
-      createPassword += numChars; // if availNumsQ selected, introduce numChars characters to createPassword
+    if (availNums) {
+      createPassword += numChars; 
     }
 
-    if (availSpecsQ) {
-      createPassword += specialChars; // if availSpecsQ selected, introduce specialChars characters to createPassword
+    if (availSpecs) {
+      createPassword += specialChars; 
     }
 
     for (i = 0; i < passLength; i++) {
